adf2dce25e2470a2235ce0c9222c93979cd74d8c
rtimoney <ronan.timoney@gmail.com>
2023-02-08 22:15:51 +0000

    Update

---
+++ helloworld.py
@@ -0,0 +1,47 @@
+print('Hello World!')
+
+# comments are declared using a hashkey
+
+# declaring a valuable and initializing (assigning a value)
+a = 8
+
+# printing the variable a will display its value in the terminal/console
+print (a)
+
+# in Python, multiple variables can be assigned in sequence
+b = c = d = 234
+
+print (b)
+print (c)
+print (d)
+
+# in Python, variables are not typecast
+
+# int
+message  = "I am an integer"
+print (message)
+print (b)
+
+#string
+message  = "I am now a string"
+print (message)
+b = "I'm going through chaaaaangeeesss"
+print (b)
+
+#float
+message  = "I am now a floating point"
+print (message)
+b = 234.56
+print (b)
+
+#boolean
+message  = "I am now a boolean"
+print (message)
+b  = True
+print (b)
+
+# concatinating example
+running = True
+print('Run state : ' , running)
+
+# it kis good practice to choose meaniful names for variables which allow yourself and others to easily follow code 
